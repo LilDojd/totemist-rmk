fd7b6959a9a1440814aa1b206262973ed8568eda
LilDojd <yawner@pm.me>
2025-07-28 14:01:46 +0400

    First Commit

---
+++ devenv.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+let
+  rmkit = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+    pname = "rmkit";
+    version = "0.0.16";
+
+    src = pkgs.fetchCrate {
+      inherit (finalAttrs) pname version;
+      hash = "sha256-FY5Z1k6tpfvlGWccDu8a3F7VATxORwHC5mIhHCSysIc=";
+    };
+
+    cargoHash = "sha256-hb6tZlJX1uCOc2C0yj/FKYzNzKRfefk6S3vIPWP0370=";
+    cargoDepsName = finalAttrs.pname;
+  });
+in
+{
+  dotenv.enable = true;
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+    components = [
+      "rustc"
+      "cargo"
+      "clippy"
+      "rustfmt"
+      "rust-analyzer"
+      "llvm-tools"
+    ];
+    targets = [ "thumbv7em-none-eabihf" ];
+  };
+
+  packages = [
+    pkgs.git
+    pkgs.cargo-make
+    pkgs.flip-link
+    rmkit
+  ];
+}
